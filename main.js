1d7b84ee544f6eb02da41bc38dcfe24168cc62a7
Tara <tara.czoschke@yahoo.com>
2023-11-20 18:05:26 +0700

    simplified js/css

--- main.js
+++ main.js
@@ -12,128 +12,53 @@ if (targetContainer.is(":visible")) {
 }
 });
 
-$(".main-nav-container button").on("click", function() {
-$(this).toggleClass("scaled primary");
-$(".main-nav-container button").not(this).removeClass("scaled primary");
-});
-
-
-$("#sentiment button").each(function() {
-$(this).on("mouseenter", function() {
-    if (!$(this).hasClass("clicked")) {
-        var bgColorClass = $(this).data("bgcolor-class");
-        $(this).addClass(bgColorClass);
-    }
-}).on("mouseleave", function() {
-    if (!$(this).hasClass("clicked")) {
-        var bgColorClass = $(this).data("bgcolor-class");
-        $(this).removeClass(bgColorClass);
-    }
-}).on("click", function() {
-    $(this).toggleClass("clicked scaled");
-}).on("dblclick", function() {
-    $(this).removeClass("clicked scaled");
-    var bgColorClass = $(this).data("bgcolor-class");
-    $(this).removeClass(bgColorClass);
-});
-});
-
-$("#country button, #topic button").each(function() {
-    $(this).on("mouseenter", function() {
+function handleButtonHover(button) {
+    var bgColorClass = button.data("bgcolor-class") || 'accent';
+    button.on("mouseenter", function() {
         if (!$(this).hasClass("clicked")) {
-            $(this).addClass('accent');
+            $(this).addClass(bgColorClass);
         }
     }).on("mouseleave", function() {
         if (!$(this).hasClass("clicked")) {
-            $(this).removeClass('accent');
+            $(this).removeClass(bgColorClass);
         }
-    }).on("click", function() {
+    });
+}
+
+function handleButtonClick(button) {
+    button.on("click", function() {
         $(this).toggleClass("clicked scaled");
-    }).on("dblclick", function() {
-        $(this).removeClass("clicked scaled");
-        $(this).removeClass('accent');
+        var bgColorClass = $(this).data("bgcolor-class") || 'accent';
+        if ($(this).hasClass("clicked")) {
+            $(this).addClass(bgColorClass);
+        } else {
+            $(this).removeClass(bgColorClass);
+        }
     });
-    });
+}
 
-    $('.circle-button-container').hide()
-    $('.rate-button').on("click", function () {
-        $('.circle-button-container').slideToggle()
-    })
+$(".main-nav-container button").on("click", function() {
+    $(this).toggleClass("scaled primary");
+    $(".main-nav-container button").not(this).removeClass("scaled primary");
+});
+
+$("#sentiment button, #country button, #topic button").each(function() {
+    handleButtonHover($(this));
+    handleButtonClick($(this));
+});
+
+
+    // $('.circle-button-container').hide()
+    // $('.rate-button').on("click", function () {
+    //     $('.circle-button-container').slideToggle()
+    // })
     
-// $(".tags").hide();
-
-// const colors = [
-//   'rgba(216, 30, 50, 1)',
-//   'rgba(235, 100, 20, 1)',
-//   'rgba(260, 200, 10, 1)',
-//   'rgb(30, 190, 100, 1)',
-// ];
-
-// // button case 
-
-// $('button')  .each(function () {
-//     const buttonText = $(this).text();
-//     const lowercased = buttonText.toLowerCase();
-//     $(this).text(lowercased);
-// });
-
-// // nav buttons
-// $("nav > button")
-//   .on("mouseenter", function () {
-//     $(this).addClass("primary");
-//     $(this).css("transform", "scale(1.05)");
-//     $(this).css("cursor", "pointer");
-//   })
-//   .on("mouseleave", function () {
-//     if (!$(this).hasClass("clicked")) {
-//       $(this).removeClass("primary");
-//       $(this).css("transform", "scale(1)");
-//     }
-//   })
-//   .on("click", function () {
-//     $(this).toggleClass("clicked");
-//     const associatedTags = $(this).next('.tags');
-//     associatedTags.slideToggle();
-//   })
-
-
-
-// // tags buttons
-// $(".tags > button").each(function (index) {
-//   $(this).data('default-bg-color', colors[index -1]);
-
-//   $(this).on("mouseenter", function () {
-//     if (!$(this).hasClass("clicked")) {
-//       $(this).css('backgroundColor', colors[index -1]);
-//       $(this).css("transform", "scale(1.05)");
-//       $(this).css("cursor", "pointer");
-//     }
-//   }).on("mouseleave", function () {
-//     if (!$(this).hasClass("clicked")) {
-//       $(this).css('backgroundColor', '');
-//       $(this).css("transform", "scale(1)");
-//     }
-//   }).on("click", function () {
-//     $(this).toggleClass("clicked");
-//   });
-// });
-
-// // special tag
-// $(".tags.hidden:eq(0) > button:first, .tags.hidden:eq(1) > button:first, .tags.hidden:eq(2) > button:first").addClass('accent');
 
 // // card click and hover 
 // $(".card").on("click", function () {
 //   const href = $(this).find("a").attr("href");
 //   window.location.href = href;
-// }).hover(
-//   function () {
-//     $(this).css("transform", "scale(1.05)");
-//     $(this).css("cursor", "pointer");
-//   },
-//   function () {
-//     $(this).css("transform", "scale(1)");
-//   }
-// );
+// })
 
 // // truncate 
 // const truncateText = () => {
