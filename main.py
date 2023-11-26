ad0e5f113e7d44026c0cb4f729a970010a82c2ee
Peter Menhinick <peterjmen@gmail.com>
2023-11-26 14:38:16 +0700

    fixed python version

--- main.py
+++ main.py
@@ -56,63 +56,13 @@ async def redirect_to_docs():
 async def root():
     return {'message': 'Go to /docs for API documentation'}
 
-@app.post("/articles/", operation_id="create_articles")
-async def create_articles(request: UserRequest = Depends()):
-    country_code = request.country if request.country else None  # Use None for a default "all" behavior
-    category_code = request.category if request.category else "general"  # Default to "general" if not provided
-    sentiment = request.sentiment  # Directly use sentiment from request
-
-    # If country code is an empty string, don't pass it to the NewsAPI client
-    params = {
-        "language": 'en',
-        "page_size": 100
-    }
-    if country_code:
-        params["country"] = country_code
-    if category_code:
-        params["category"] = category_code
-
-    response = newsapi.get_top_headlines(**params)
-
-    articles = response.get("articles", [])
-    filtered_articles = []
-
-    for article in articles:
-        sentiment_value = calculate_sentiment(article["title"])
-        article_sentiment = sentiment_to_category(sentiment_value)
-
-        # Filter by sentiment if provided
-        if sentiment and article_sentiment != sentiment:
-            continue
-
-        article_info = {
-            "headline": article["title"],
-            "date": datetime.strptime(article["publishedAt"], "%Y-%m-%dT%H:%M:%SZ").strftime("%b %d, %Y %H:%M:%S"),
-            "author": article.get("author"),
-            "source_name": article["source"]["name"] if article.get("source") else None,
-            "content": article.get("content"),
-            "url": article["url"],
-            "urlToImage": article.get("urlToImage"),
-            "sentiment": article_sentiment
-        }
-
-        filtered_articles.append(article_info)
-
-    # Write to JSON file
-    with open("articles.json", "w") as file:
-        json.dump(filtered_articles, file)
-
-    # Return the JSON response
-    return JSONResponse(content={"articles": filtered_articles})
-
 @app.get("/articles/", operation_id="get_articles")
 async def get_articles(
     country: Optional[str] = None,  # Use query parameters, not Form
     category: Optional[str] = None,
     sentiment: Optional[str] = None
 ):
-    # The logic here will be similar to the POST endpoint,
-    # but adapted to use query parameters instead of form data.
+
     country_code = country if country else None  # Use None for a default "all" behavior
     category_code = category if category else "general"  # Default to "general" if not provided
 
@@ -152,8 +102,8 @@ async def get_articles(
 
         filtered_articles.append(article_info)
 
-    # No need to write to a JSON file unless you specifically need it
-    # Simply return the filtered articles as a JSON response
+
+    # Return the filtered articles as a JSON response
     return JSONResponse(content={"articles": filtered_articles})
 
 # Run the application locally to test:
